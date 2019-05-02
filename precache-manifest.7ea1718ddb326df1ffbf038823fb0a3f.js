09dad161eb0211d209ab4c9a75903d947726d845
Robert Springer <springerdrums@gmail.com>
2019-05-01 18:57:58 -0700

    Updates

---
+++ precache-manifest.7ea1718ddb326df1ffbf038823fb0a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceef9724c5c633ddd1d39a8e84eddca1",
+    "url": "/dev-portfolio/index.html"
+  },
+  {
+    "revision": "03382b5171a8499cdbf4",
+    "url": "/dev-portfolio/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/dev-portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "03382b5171a8499cdbf4",
+    "url": "/dev-portfolio/static/js/main.274b7fd1.chunk.js"
+  },
+  {
+    "revision": "88b939c8ada9983f3096",
+    "url": "/dev-portfolio/static/js/runtime~main.e91790c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dev-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
